9a443d5067f251c0e211afc91dc34caa6c138c97
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-02-10 20:12:50 +0100

    Added zuul logging pre-filter
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

---
+++ src/main/java/org/wikitolearn/gateway/pwa/filters/pre/LoggingPreFilter.java
@@ -0,0 +1,40 @@
+package org.wikitolearn.gateway.pwa.filters.pre;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class LoggingPreFilter extends ZuulFilter {
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 1;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
+
+    log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+    return null;
+  }
+}
