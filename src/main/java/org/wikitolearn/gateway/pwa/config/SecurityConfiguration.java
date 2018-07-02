3901ca29e6f3983e3c7b62f2901f8a57e40ca97d
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-07-02 17:08:37 +0200

    Changed oauth2 configs: RSA key is now automatically retrieved
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/main/java/org/wikitolearn/gateway/pwa/config/SecurityConfiguration.java
+++ src/main/java/org/wikitolearn/gateway/pwa/config/SecurityConfiguration.java
@@ -1,17 +1,12 @@
 package org.wikitolearn.gateway.pwa.config;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,11 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -69,34 +59,5 @@ public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
   @Override
   public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
     resources.resourceId(resourceServerProperties.getResourceId());
-  }
-
-  @Bean
-  @ConditionalOnProperty("security.oauth2.resource.jwt.key-uri")
-  public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
-    return new JwtTokenStore(jwtAccessTokenConverter);
-  }
-
-  @Bean
-  @ConditionalOnProperty("security.oauth2.resource.jwt.key-uri")
-  public JwtAccessTokenConverter jwtAccessTokenConverter() {
-    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-    converter.setVerifierKey(getKeyFromAuthorizationServer());
-    return converter;
-  }
-
-  private String getKeyFromAuthorizationServer() {
-    return Optional.ofNullable(
-      new RestTemplate()
-        .exchange(
-          resourceServerProperties.getJwt().getKeyUri(),
-          HttpMethod.GET,
-          new HttpEntity<Void>(new HttpHeaders()),
-          Map.class
-        )
-        .getBody()
-        .get("public_key"))
-    .map(publicKey -> String.format("-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----", publicKey))
-    .orElse(resourceServerProperties.getJwt().getKeyValue());
   }
 }
